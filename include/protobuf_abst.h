5d0ffebb5d232a895e136735acec037645664b2b
fasagidagrc <fatih.asagidag@riddleandcode.com>
2024-02-12 13:03:32 +0300

    Added nanopb branch of librddl

--- include/protobuf_abst.h
+++
@@ -1,4 +0,0 @@
-void* fullfill_planetmintgo_machine_metadata(char* cid, char* gps, char* assetdef, char* device);
-void* fullfill_planetmintgo_machine_machine(void* metadata_ptr, char* signature_hex);
-void* fullfill_planetmintgo_machine_msgAttestMachine(void* machine_ptr);
-int bind_msgAttestMachine_to_anyMsg(void* anyMsg, void* machineMsg);
